30b7b71d61a312f5fbfcc8d2498cf8ca7035aac2
joannaCL <joanna.kruczkowska@coderslab.pl>
2018-10-18 12:23:55 +0200

    Refactor code in app.js file

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 'use strict';
 
 //zapewniemy, żeby załadowała się najpierw cała zawartość kodu HTML, zanim zaczniemy wykonywać jakieś działania:
-document.addEventListener('DOMContentLoaded', function() {
-      
+document.addEventListener('DOMContentLoaded', function () {
+
       //sprawdzamy czy plik został prawidłowo podpięty:
       console.log('Sierotka ma rysia');
 
@@ -21,29 +21,29 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log(dropdownTab.style);
 
       //po najechaniu myszką pojawi się menu rozwijane:
-      dropdownTab.addEventListener('mouseover', function() {
-      	dropdownListElements.style.display = 'block';
+      dropdownTab.addEventListener('mouseover', function () {
+            dropdownListElements.style.display = 'block';
       });
 
       //gdy kursor znajduje się poza menu - lista rozwijana znika:
-      dropdownTab.addEventListener('mouseout', function() {
-      	dropdownListElements.style.display = 'none';
+      dropdownTab.addEventListener('mouseout', function () {
+            dropdownListElements.style.display = 'none';
       });
 
-//Zadanie 2
+      //Zadanie 2
 
       var buttons = document.querySelectorAll('.read-more');
 
       function showHide() {
 
-      var textArea = this.previousElementSibling;
-   
-      if (textArea.style.display === 'none' || textArea.style.display === '') {
-            textArea.style.display = 'block';
-            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-      } else {
-            textArea.style.display = 'none';
-            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+            var textArea = this.previousElementSibling;
+
+            if (textArea.style.display === 'none' || textArea.style.display === '') {
+                  textArea.style.display = 'block';
+                  this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+            } else {
+                  textArea.style.display = 'none';
+                  this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
             }
       }
 
@@ -51,4 +51,4 @@ document.addEventListener('DOMContentLoaded', function() {
             buttons[i].addEventListener('click', showHide);
       }
 
-    });
+});
